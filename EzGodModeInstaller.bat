ef6fc5eb7da9321ddebf4fa1db4e4d5d1e632064
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2021-02-17 19:04:41 +0100

    Update and rename EzGodMode.bat to EzGodModeInstaller.bat

---
+++ EzGodModeInstaller.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+ECHO Installing EzGodMode
+ECHO https://github.com/devilAPI/EzGodMode
+ECHO.
+ECHO Click any Button to confirm installation.
+PAUSE
+ECHO.
+ECHO INSTALLING...
+MKDIR EzGodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
+ECHO Thank you for using EzGodMode!
+ECHO Click any Button to close this Window.
+PAUSE
+EXIT
